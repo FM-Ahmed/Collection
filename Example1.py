5e4130188158adf838203e091ddabfc800304d66
Fahim Masud Ahmed <128718838+FM-Ahmed@users.noreply.github.com>
2023-06-25 01:05:38 +0200

    Update Example1.py

--- Example1.py
+++ Example1.py
@@ -1,4 +1,6 @@
 import numpy as np
+from functions import *
+
 n = 11 # some arbitrary number you want to check, code might take a while if n is too large... Must be a whole number, i.e. an integer.
 check_prime(n)
 # returns '11 is a prime!'.
