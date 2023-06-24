7064c3edd0b5c48aae1d5cb7f7fc69ad15e0d0d5
Fahim Masud Ahmed <128718838+FM-Ahmed@users.noreply.github.com>
2023-06-25 01:05:05 +0200

    Update Example1.py

--- Example1.py
+++ Example1.py
@@ -3,3 +3,6 @@ n = 11 # some arbitrary number you want to check, code might take a while if n i
 check_prime(n)
 # returns '11 is a prime!'.
 # if input n was not a prime number, e.g 12, it would return '12 is not a prime...'.
+
+number_of_primes(n)
+# returns 'There are 5 prime numbers between 1 and 11.'
