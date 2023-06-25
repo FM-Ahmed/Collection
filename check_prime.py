bc62f06c3f8224c076c493e021d18597529c07b6
Fahim Masud Ahmed <128718838+FM-Ahmed@users.noreply.github.com>
2023-06-25 13:48:39 +0200

    Update and rename functions.py to check_prime.py

--- check_prime.py
+++ check_prime.py
@@ -33,22 +33,3 @@ def check_prime(n):
         is_prime = False  
         
     return is_prime
-
-def primes(n):
-        # Input:
-            # n: some arbitrary whole number.
-        # Output:
-            # prime_numbers: a list of prime numbers between 1 and n.
-        
-    if type(n) != int:
-        raise ValueError('Input {} is not a whole number... Please provide an integer.'.format(n))
-        
-    nums = np.arange(1,n+1,1)
-    
-    prime_numbers = []
-    for value in nums:
-        result = check_prime(int(value))
-        if result == True:
-            prime_numbers.append(value) 
-            
-    return prime_numbers
