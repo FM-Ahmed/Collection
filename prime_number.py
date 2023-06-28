c50336defc5025d7e4e3d963375488c741caa042
Fahim Masud Ahmed <128718838+FM-Ahmed@users.noreply.github.com>
2023-06-28 23:02:18 +0200

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,6 +1,12 @@
-import numpy as np
-
 def prime_number(x):
+        # Input:
+            # n: some arbitrary whole number.
+        # Output:
+            # is_prime: returns either True or False.
+        
+    if type(n) != int:
+        raise ValueError('Input {} is not a whole number... Please provide an integer.'.format(n))
+        
     rng = np.arange(1, x + 1, 1)
     c = 0
     for num in rng:
