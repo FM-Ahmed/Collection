91942730eee6b1f091f3678cb3db5d5d5cecc33e
Fahim Masud Ahmed <128718838+FM-Ahmed@users.noreply.github.com>
2024-01-26 23:57:44 +0100

    Update check_prime.py

--- check_prime.py
+++ check_prime.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 def check_prime(x):
     '''
         Input:
@@ -7,17 +5,14 @@ def check_prime(x):
         Returns:
             bool: True if prime, False if not
     '''
-    if not isinstance(x, int) or x <= 1:
+    if not isinstance(x, int) or x < 1:
         raise ValueError(f'Input {x} is invalid. Please provide a positive integer above 1.')
         
-    if x % 2 == 0 and x != 2:
+    if (x % 2 == 0 and x != 2) or x == 1:
         return False
         
-    rng = np.arange(1, np.sqrt(x) + 1, 1)
-    c = 0
+    rng = range(3, int(np.sqrt(x) + 1), 2)
     for num in rng:
         if x % num == 0: 
-            c += 1
-            if c >= 3:
-                break
-    return c == 2
+            return False
+    return True
