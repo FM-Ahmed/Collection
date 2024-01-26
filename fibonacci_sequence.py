af1289dad3c8c8cab32dff55876ce99cbe02f33f
Fahim Masud Ahmed <128718838+FM-Ahmed@users.noreply.github.com>
2024-01-27 00:07:41 +0100

    Update fibonacci_sequence.py

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,9 +1,14 @@
 def fibonacci_sequence(n):
-  # Input
-    # n: integer, how many numbers of the fibonacci sequence you are interested in.
-  # Output
-    # fibonacci: a list with length (n+1). This is because the sequence starts with index 0.
-  
+    '''
+    Generates Fibonaccu numbers to the n-th term
+    Input
+    n (int): how many numbers of the fibonacci sequence you are interested in.
+    Output
+    fibonacci (list): length of returned list is n+1 because the sequence starts with index 0.
+    '''
+    if n < 1:
+        return []
+    
     fibonacci = [0, 1]
     for i in range(1, n):
         new_val = fibonacci[i] + fibonacci[i-1]
