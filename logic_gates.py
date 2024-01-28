026fe22196b023e6c2470ed3b8e00c2baf82fe45
Fahim Masud Ahmed <128718838+FM-Ahmed@users.noreply.github.com>
2024-01-28 22:50:19 +0100

    Update logic_gates.py

--- logic_gates.py
+++ logic_gates.py
@@ -9,30 +9,30 @@ class logic_gates:
     def not_gate(self, binary_inputs):
         self.validate_binary(binary_inputs)
         if len(binary_inputs) == 1:
-            return 1 if int(binary_inputs) == 0 else 0
+            return '1' if int(binary_inputs) == 0 else '0'
         else:
             raise ValueError(f'String cannot {len(binary_inputs)} characters. Must be 1 character long.')
         
     def and_gate(self, binary_inputs):
         self.validate_binary(binary_inputs)
-        return 0 if not all(i in '1' for i in binary_inputs) else 1
+        return '0' if not all(i in '1' for i in binary_inputs) else '1'
     
     def nand_gate(self, binary_inputs):
         self.validate_binary(binary_inputs)
-        return 1 if not all(i in '1' for i in binary_inputs) else 0
+        return '1' if not all(i in '1' for i in binary_inputs) else '0'
         
     def or_gate(self, binary_inputs):
         self.validate_binary(binary_inputs)
-        return 1 if any (i in '1' for i in binary_inputs) else 0
+        return '1' if any (i in '1' for i in binary_inputs) else '0'
     
     def xor_gate(self, binary_inputs):
         self.validate_binary(binary_inputs)
-        return 1 if sum([int(i) for i in binary_inputs]) in (1, len(binary_inputs)-1) else 0
+        return '1' if sum([int(i) for i in binary_inputs]) in (1, len(binary_inputs)-1) else '0'
     
     def nor_gate(self, binary_inputs):
         self.validate_binary(binary_inputs)
-        return 1 if sum([int(i) for i in binary_inputs]) == 0 else 0
+        return '1' if sum([int(i) for i in binary_inputs]) == 0 else '0'
     
     def xnor_gate(self, binary_inputs):
         self.validate_binary(binary_inputs)
-        return 1 if sum([int(i) for i in binary_inputs]) in (0, len(binary_inputs)) else 0
+        return '1' if sum([int(i) for i in binary_inputs]) in (0, len(binary_inputs)) else '0'
